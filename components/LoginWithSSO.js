c393d7fbabfc62f661cf294f625efdb972a0fda3
inertiawhoosh <victorialgodfrey@gmail.com>
2022-07-06 17:10:22 -0600

    revert to current home page and lint

--- components/LoginWithSSO.js
+++ components/LoginWithSSO.js
@@ -87,7 +87,7 @@ export default class LoginWithSSO extends React.Component {
                   </div>
                 </div>
               ) : (
-               <div></div>
+                <div></div>
               )}
 
               <div>
