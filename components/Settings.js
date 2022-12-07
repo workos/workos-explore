4e4b914b2199bb66c8c4a54996c434b6306cbce1
Rob Frampton <rob@workos.com>
2022-12-07 14:32:57 -0500

    Add ability to view audit logs with Admin Portal

--- components/Settings.js
+++ components/Settings.js
@@ -96,6 +96,34 @@ export default function Settings(props) {
               </div>
             </form>
           </section>
+
+          <section aria-labelledby="payment-details-heading">
+            <form onSubmit={props.onSubmit.bind(this, 'audit_logs')}>
+              <div className="shadow sm:rounded-md sm:overflow-hidden">
+                <div className="bg-white py-6 px-4 sm:p-6">
+                  <div>
+                    <h2
+                      id="payment-details-heading"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Audit Logs
+                    </h2>
+                    <p className="mt-1 text-sm text-gray-500">
+                      View Audit Log events belonging to your organization.
+                    </p>
+                  </div>
+                </div>
+                <div className="px-4 py-3 bg-gray-50 sm:px-6">
+                  <button
+                    type="submit"
+                    className="py-2 px-4 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  >
+                    View
+                  </button>
+                </div>
+              </div>
+            </form>
+          </section>
         </div>
       </div>
     </main>
--- pages/api/admin-portal.js
+++ pages/api/admin-portal.js
@@ -3,6 +3,31 @@ import baseURL from '../../lib/baseURL'
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY)
 
+const createAuditLogEvents = async (organization) => {
+  for (const action of ['user.signed_in', 'user.signed_out']) {
+    await workos.auditLogs.createEvent(organization.id, {
+      action,
+      occurred_at: new Date(),
+      actor: {
+        type: 'user',
+        name: 'Jon Smith',
+        id: 'user_01GBNJC3MX9ZZJW1FSTF4C5938',
+      },
+      targets: [
+        {
+          type: 'team',
+          id: 'team_01GBNJD4MKHVKJGEWK42JNMBGS',
+        },
+      ],
+      context: {
+        location: '1.1.1.1',
+        user_agent:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+      },
+    })
+  }
+}
+
 export default async (req, res) => {
   try {
     const { intent, state } = req.body
@@ -13,6 +38,10 @@ export default async (req, res) => {
       name,
       domains,
     })
+
+    if (intent === 'audit_logs') {
+      await createAuditLogEvents(organization)
+    }
 
     const { link } = await workos.portal.generateLink({
       intent,
