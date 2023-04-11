3f1474e3da281ca3e59389a0fb5bca6e6efa4713
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2023-04-11 15:10:50 -0600

    Trigger vercel redeploy

--- components/LoginWithSSO.js
+++ components/LoginWithSSO.js
@@ -28,7 +28,7 @@ export default class LoginWithSSO extends React.Component {
                     id="email"
                     name="email"
                     type="text"
-                    placeholder="Enter email address"                    
+                    placeholder="Enter email address "                    
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   />
